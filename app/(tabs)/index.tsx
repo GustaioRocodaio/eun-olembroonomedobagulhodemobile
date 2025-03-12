36a8a16c22b89afc96326b78b9f77541caf88af2
FantasmeiroFantasma <163547571+GustaioRocodaio@users.noreply.github.com>
2025-03-12 08:54:56 -0300

    Add files via upload

---
+++ app/(tabs)/index.tsx
@@ -0,0 +1,69 @@
+import { Image, StyleSheet, Platform } from 'react-native';
+
+import { HelloWave } from '@/components/HelloWave';
+import ParallaxScrollView from '@/components/ParallaxScrollView';
+import { ThemedText } from '@/components/ThemedText';
+import { ThemedView } from '@/components/ThemedView';
+
+export default function HomeScreen() {
+  return (
+    <ParallaxScrollView
+      headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
+      
+      headerImage={
+        <Image
+
+        
+          source={require('@/assets/images/unnamed.jpg')}
+          style={styles.reactLogo}
+        />
+      }>
+      <ThemedView style={styles.titleContainer}>
+        <ThemedText type="title">Bem Vindo(a) ao meu site... ao menos parece um</ThemedText>
+        <HelloWave />
+      </ThemedView>
+      <ThemedView style={styles.stepContainer}>
+        <ThemedText type="subtitle">meu nome é *unknown*</ThemedText>
+        <ThemedText>
+           <ThemedText type="defaultSemiBold">E aqui é o meu site</ThemedText> 
+          <ThemedText type="defaultSemiBold">
+         
+          </ThemedText>{' '}
+        </ThemedText>
+      </ThemedView>
+      <ThemedView style={styles.stepContainer}>
+        <ThemedText type="subtitle">Especialidades:</ThemedText>
+        <ThemedText>
+nehuma        </ThemedText>
+      </ThemedView>
+      <ThemedView style={styles.stepContainer}>
+        <ThemedText type="subtitle">Como entrar em contato:</ThemedText>
+        <ThemedText>
+NÃO ENTRE          <ThemedText type="defaultSemiBold"></ThemedText> 
+          <ThemedText type="defaultSemiBold"></ThemedText> 
+          <ThemedText type="defaultSemiBold"></ThemedText> 
+          <ThemedText type="defaultSemiBold"></ThemedText>
+        </ThemedText>
+      </ThemedView>
+    </ParallaxScrollView>
+  );
+}
+
+const styles = StyleSheet.create({
+  titleContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    gap: 8,
+  },
+  stepContainer: {
+    gap: 8,
+    marginBottom: 8,
+  },
+  reactLogo: {
+    height: 251,
+    width: 353,
+    bottom: 0,
+    left: 0,
+    position: 'absolute',
+  },
+});
